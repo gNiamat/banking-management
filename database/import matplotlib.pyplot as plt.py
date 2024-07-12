5ff3a2ef0a5fbae2d1e675d3ef119471ce2f0b6a
gNiamat <114097794+gNiamat@users.noreply.github.com>
2024-07-12 20:35:50 +0530

    Initial commit

---
+++ database/import matplotlib.pyplot as plt.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import torch
+import torch.nn as nn
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import transformers
+from transformers import AutoModel, BertTokenizerFast
+from sklearn.decomposition import PCA
+import tensorflow_hub as hub
+from pycaret.classification import * 
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import plot_confusion_matrix
+#from googletrans import Translator
+plt.style.use('ggplot')
+plt.rcParams['font.family'] = 'sans-serif' 
+plt.rcParams['font.serif'] = 'Ubuntu' 
+plt.rcParams['font.monospace'] = 'Ubuntu Mono' 
+plt.rcParams['font.size'] = 14 
+plt.rcParams['axes.labelsize'] = 12 
+plt.rcParams['axes.labelweight'] = 'bold' 
+plt.rcParams['axes.titlesize'] = 12 
+plt.rcParams['xtick.labelsize'] = 12 
+plt.rcParams['ytick.labelsize'] = 12 
+plt.rcParams['legend.fontsize'] = 12 
+plt.rcParams['figure.titlesize'] = 12 
+plt.rcParams['image.cmap'] = 'jet' 
+plt.rcParams['image.interpolation'] = 'none' 
+plt.rcParams['figure.figsize'] = (10, 10
+                                 ) 
+plt.rcParams['axes.grid']=False
+plt.rcParams['lines.linewidth'] = 2 
+plt.rcParams['lines.markersize'] = 8
+colors = ['xkcd:pale range', 'xkcd:sea blue', 'xkcd:pale red', 'xkcd:sage green', 'xkcd:terra cotta', 'xkcd:dull purple', 'xkcd:teal', 'xkcd: goldenrod', 'xkcd:cadet blue',
+'xkcd:scarlet']
+bbox_props = dict(boxstyle="round,pad=0.3", fc=colors[0], alpha=.5)
+import pandas as pd
+import pycaret 
